88d6c66d9aab612d1fd93f31c9c1300f873b8091
Nicolas084505 <nicolassanxd@gmail.com>
2025-02-16 16:53:42 -0600

    comCal
    cambios drag ad drop y mejora de la vista, español

--- config/global.php
+++ config/global.php
@@ -3,7 +3,7 @@
 define('DB_NAME_MYSQL', 'imprenta');
 define('DB_HOST_MYSQL', 'localhost');
 define('DB_USER_MYSQL', 'root');
-define('DB_PASSWORD_MYSQL', 'lulu2003');
+define('DB_PASSWORD_MYSQL', 'Borona12');
 //Change the password and the user if you needed
 
 $db = new mysqli(DB_HOST_MYSQL, DB_USER_MYSQL, DB_PASSWORD_MYSQL, DB_NAME_MYSQL);
