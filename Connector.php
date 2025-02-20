49dcca461f646db1e6713f6a0ed20b10b75ea0f8
Sebasrp840 <133722863+Sebasrp840@users.noreply.github.com>
2025-02-20 15:05:25 -0600

    Update Connector.php

--- Connector.php
+++ Connector.php
@@ -4,9 +4,9 @@ $user = "Sebastian";
 $password = "Bootysniper1311";
 $database = "imprenta4-0";
 
+// Inicializa la conexión sin SSL
 $conn = mysqli_init();
-mysqli_ssl_set($conn, NULL, NULL, "/home/site/wwwroot/db/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
-mysqli_real_connect($conn, $host, $user, $password, $database, 3306, NULL, MYSQLI_CLIENT_SSL);
+mysqli_real_connect($conn, $host, $user, $password, $database, 3306, NULL, 0); // Cambia MYSQLI_CLIENT_SSL a 0
 
 if (mysqli_connect_errno()) {
     die("Error de conexión: " . mysqli_connect_error());
