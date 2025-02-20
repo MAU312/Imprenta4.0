b3c09bc74e75c629baaedc9363d929bec5ed0efe
Sebasrp840 <133722863+Sebasrp840@users.noreply.github.com>
2025-02-20 03:25:28 -0600

    Update global.php

--- config/global.php
+++ config/global.php
@@ -3,7 +3,7 @@
 define('DB_NAME_MYSQL', 'imprenta');
 define('DB_HOST_MYSQL', 'imprenta4-0.mysql.database.azure.com'); 
 define('DB_USER_MYSQL', 'Sebastian@imprenta4-0');
-define('DB_PASSWORD_MYSQL', 'tu_password'); 
+define('DB_PASSWORD_MYSQL', 'Bootysniper1311'); 
 //Change the password and the user if you needed
 
 $db = new mysqli(DB_HOST_MYSQL, DB_USER_MYSQL, DB_PASSWORD_MYSQL, DB_NAME_MYSQL);
