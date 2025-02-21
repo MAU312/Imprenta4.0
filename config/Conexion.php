fa9444e3a53be15570691753af8ea6142bb88afa
Sebasrp840 <133722863+Sebasrp840@users.noreply.github.com>
2025-02-21 04:11:48 -0600

    Conexion prueba

--- config/Conexion.php
+++ config/Conexion.php
@@ -4,26 +4,21 @@ require_once "global.php";
 
 class Conexion
 {
-    function __construct()
-    {
-        # code...
-    }
-    
     public static function conectar(){
-        // Conexión MySQL
-        
-        // Aquí hace la conexión a la base de datos, las variables se encuentran en la clase global
+        // conexion mysql
         try {
-            $options = [
-                PDO::MYSQL_ATTR_SSL_CA => SSL_CERT_PATH // Agrega la ruta al certificado SSL
-            ];
-
+            $options = array(
+                PDO::MYSQL_ATTR_SSL_CA => SSL_CERT_PATH, // Ruta del certificado
+                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false // Cambia a true si deseas verificar el servidor
+            );
+            
             $cn = new PDO("mysql:host=".DB_HOST_MYSQL.
             ";dbname=".DB_NAME_MYSQL.
             ";charset=utf8",
             DB_USER_MYSQL,
             DB_PASSWORD_MYSQL,
-            $options); // Asegúrate de pasar las opciones
+            $options); // Agrega las opciones SSL aquí
+
             $cn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             return $cn;
         } catch (PDOException $ex) {
@@ -31,4 +26,17 @@ class Conexion
         }
     }
 }
+
+// Línea de código que ayuda a saber si está correctamente conectada la BD
+/*
+$conexion = Conexion::conectar();
+
+if ($conexion) {
+    var_dump($conexion);
+    echo "Conexión exitosa a la base de datos";
+} else {
+    echo "Error al conectar a la base de datos";
+}
+*/
+
 
--- config/global.php
+++ config/global.php
@@ -2,7 +2,7 @@
 // CONSTANTES
 define('DB_NAME_MYSQL', 'imprenta');
 define('DB_HOST_MYSQL', 'imprenta4-0.mysql.database.azure.com'); // Cambia esto
-define('DB_USER_MYSQL', 'Sebastian@imprenta4-0.mysql.database.azure.com'); // Cambia esto
+define('DB_USER_MYSQL', 'Sebastian'); // Cambia esto
 define('DB_PASSWORD_MYSQL', 'Bootysniper1311'); // Cambia esto
 define('SSL_CERT_PATH', __DIR__ . '/certs/DigiCertGlobalRootCA.crt.pem');
 ?>
