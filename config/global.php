559cae55414b951e1048bd89cd4d69612ba0eed8
Sebasrp840 <133722863+Sebasrp840@users.noreply.github.com>
2025-02-21 03:07:14 -0600

    Update global.php

--- config/global.php
+++ config/global.php
@@ -4,7 +4,7 @@ define('DB_NAME_MYSQL', 'imprenta');
 define('DB_HOST_MYSQL', 'imprenta4-0.mysql.database.azure.com'); // Cambiado de localhost a Azure
 define('DB_USER_MYSQL', 'Sebastian@imprenta4-0.mysql.database.azure.com'); // Asegúrate de usar el usuario completo
 define('DB_PASSWORD_MYSQL', 'Bootysniper1311'); // Cambia la contraseña si es necesario
-define('SSL_CERT_PATH', __DIR__ . '/certificados/DigiCertGlobalRootCA.crt.pem');
+define('SSL_CERT_PATH', __DIR__ . '/certs/DigiCertGlobalRootCA.crt.pem');
 
 $db = new mysqli(DB_HOST_MYSQL, DB_USER_MYSQL, DB_PASSWORD_MYSQL, DB_NAME_MYSQL, 3306, NULL, MYSQLI_CLIENT_SSL);
 
