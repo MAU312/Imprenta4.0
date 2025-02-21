0a1f4811de8d48874d822d1f04b35379be9b5862
Sebasrp840 <133722863+Sebasrp840@users.noreply.github.com>
2025-02-21 03:24:15 -0600

    Update Connector.php

--- Connector.php
+++ Connector.php
@@ -3,11 +3,10 @@ $host = "imprenta4-0.mysql.database.azure.com";
 $user = "Sebastian@imprenta4-0.mysql.database.azure.com";
 $password = "Bootysniper1311";
 $database = "imprenta";
-$sslCertPath = "C:\\xampp\\htdocs\\Imprenta4.0\\certs\\DigiCertGlobalRootCA.crt.pem";
 
 // Inicializa la conexión sin SSL
-mysqli_ssl_set($conn, NULL, NULL, $sslCertPath, NULL, NULL); // Ruta al certificado
-mysqli_real_connect($conn, $host, $user, $password, $database, 3306, NULL, MYSQLI_CLIENT_SSL);
+$conn = mysqli_init();
+mysqli_real_connect($conn, $host, $user, $password, $database, 3306, NULL, 0); // Cambia MYSQLI_CLIENT_SSL a 0
 
 if (mysqli_connect_errno()) {
     die("Error de conexión: " . mysqli_connect_error());
