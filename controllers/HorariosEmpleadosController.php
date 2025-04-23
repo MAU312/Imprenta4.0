5bf308ac6ad4a20bcf9063e20cfa9d3b620e48bb
MAU312 <106779980+MAU312@users.noreply.github.com>
2025-04-23 00:32:26 -0600

    b

--- controllers/HorariosEmpleadosController.php
+++ controllers/HorariosEmpleadosController.php
@@ -1,7 +1,7 @@
 <?php
 require_once '../models/HorariosEmpleados.php'; // Asegúrate de que este archivo esté en la ubicación correcta
 
-echo "Hola desde el controlador";
+//echo "Hola desde el controlador";
 
 // Verificar si se ha enviado la operación deseada
 if (isset($_GET["op"])) {
