2db78ec53cdd9e7cc7e2be7f7f5b5949031b00aa
MAU312 <106779980+MAU312@users.noreply.github.com>
2025-04-22 23:53:25 -0600

    global

--- config/global.php
+++ config/global.php
@@ -13,9 +13,11 @@ define('DB_PASSWORD_MYSQL', 'Bootysniper1311');
 define('SSL_CERT_PATH', __DIR__ . '/certs/DigiCertGlobalRootCA.crt.pem');
 //Change the password and the user if you needed
 
+/*
 $db = new mysqli(DB_HOST_MYSQL, DB_USER_MYSQL, DB_PASSWORD_MYSQL, DB_NAME_MYSQL);
 
 // Verificar la conexión
 if ($db->connect_error) {
     die("Connection failed: " . $db->connect_error); // Mensaje de error detallado
 }
+*/
